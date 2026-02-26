0359d3645f6c0b430a6e0c801e3ff2396b5d7ca3
Campocruz <cosmacampo93@yahoo.it>
2026-02-26 23:02:27 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Definizione Variabili
+
+const cost_km = 0.21;
+const minor = 18;
+const over = 65;
+const discount_minors = 20;
+const discount_over = 40;
+const user_adge = 70;
+const user_km = 100;
+let price;
+
+if (user_adge <= minor) {
+  price = (cost_km * user_km) - ((cost_km * user_km) * discount_minors) / 100
+} else if (user_adge >= over) {
+  price = (cost_km * user_km) - ((cost_km * user_km) * discount_over) / 100
+} else {
+  price = (cost_km * user_km)
+}
+console.log(price);
