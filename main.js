98f32d1ca13997aee00d5106ca8d560db39e8160
Campocruz <cosmacampo93@yahoo.it>
2026-03-02 22:15:47 +0100

    integrate a function

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
-// Definizione Variabili
+// Tag HTML da usare
+const button = document.getElementById('btn_calc');
+const element_cost_km = document.getElementById('inputCost');
+const element_user_age = document.getElementById('inputAge');
+const element_user_km = document.getElementById('inputKm');
 
-const cost_km = 0.21;
+// Definizione Cosanti 
+// const cost_km = 0.21;
 const minor = 18;
 const over = 65;
 const discount_minors = 20;
 const discount_over = 40;
-
 let price;
-// Take Variabili from user
-const user_age = Number(prompt('Inserisci la tua età'));
-const user_km = Number(prompt('Inserisci i Km'));
 
-if (user_age <= minor) {
-  price = (cost_km * user_km) - ((cost_km * user_km) * discount_minors) / 100
-} else if (user_age >= over) {
-  price = (cost_km * user_km) - ((cost_km * user_km) * discount_over) / 100
-} else {
-  price = (cost_km * user_km)
+// Take Variabili from user with prompt
+// const user_age = Number(prompt('Inserisci la tua età'));
+// const user_km = Number(prompt('Inserisci i Km'));
+
+// if (user_age <= minor) {
+//   price = (cost_km * user_km) - ((cost_km * user_km) * discount_minors) / 100
+// } else if (user_age >= over) {
+//   price = (cost_km * user_km) - ((cost_km * user_km) * discount_over) / 100
+// } else {
+//   price = (cost_km * user_km)
+// }
+// console.log(price.toFixed(2) + '€');
+
+//# operazioni di iterazioni
+// al click del bottone 
+button.addEventListener('click', calcKm)
+
+// Funzioni
+function calcKm() {
+  const cost_km = element_cost_km.value;
+  const user_age = element_user_age.value;
+  const user_km = element_user_km.value;
+
+  if (user_age <= minor) {
+    price = (cost_km * user_km) - ((cost_km * user_km) * discount_minors) / 100
+  } else if (user_age >= over) {
+    price = (cost_km * user_km) - ((cost_km * user_km) * discount_over) / 100
+  } else {
+    price = (cost_km * user_km)
+  }
+  console.log(price.toFixed(2) + '€');
 }
-console.log(price.toFixed(2) + '€');
-
-
-// Prove 
-let live_user_age = document.forms["calculator"]["inputAdge"].value;
-console.log(live_user_age);
--- main_live.js
+++ main_live.js
@@ -3,50 +3,50 @@ Controllare la maggioranza, la minoranza o l'uguaglianza
 di due numeri inseriti dall'utente con la funzione prompt
 */
 
-// const my_age = Number(prompt('Inserisci la TUA età'));
-// const your_age = Number(prompt('Inserisci la SUA età'));
+const my_age = Number(prompt('Inserisci la TUA età'));
+const your_age = Number(prompt('Inserisci la SUA età'));
 
-// if (my_age < your_age) {
-//   console.log('La tua età è minore della sua')
-// } else if (my_age > your_age) {
-//   console.log('La tua età è maggiore della sua')
-// } else {
-//   console.log('Le vostre età suono uguali')
-// }
+if (my_age < your_age) {
+  console.log('La tua età è minore della sua')
+} else if (my_age > your_age) {
+  console.log('La tua età è maggiore della sua')
+} else {
+  console.log('Le vostre età suono uguali')
+}
 
 /*
 Il gioco del pari e dispari usando la funzione random 
 per generare un numero da 1 a 9
 */
 
-// const user_pair = Number(prompt('Pari digita 1', '1'));
-// const user_number = Number(prompt('Insert the number from 1 to 9'))
-// if (!(user_number <= 0 || user_number > 9)) {
-//   const random_number = Math.floor(Math.random() * 10);
-//   const sum_number = user_number + random_number;
-//   console.log(random_number, sum_number)
-//   if ((sum_number % 2) == 0) {
-//     console.log('La somma dei numeri è pari')
-//     if (user_pair == 1) {
-//       console.log('hai vinto tu')
-//     }
-//     else {
-//       console.log('Ha vinto il computer')
-//     }
-//   }
-//   else {
-//     console.log('La somma dei numeri è dispari')
-//     if (user_pair == 0) {
-//       console.log('hai vinto tu')
-//     }
-//     else {
-//       console.log('Ha vinto il computer')
-//     }
-//   }
-// }
-// else {
-//   console.log('Hai inserito un numero sbagliato')
-// }
+const user_pair = Number(prompt('Pari digita 1', '1'));
+const user_number = Number(prompt('Insert the number from 1 to 9'))
+if (!(user_number <= 0 || user_number > 9)) {
+  const random_number = Math.floor(Math.random() * 10);
+  const sum_number = user_number + random_number;
+  console.log(random_number, sum_number)
+  if ((sum_number % 2) == 0) {
+    console.log('La somma dei numeri è pari')
+    if (user_pair == 1) {
+      console.log('hai vinto tu')
+    }
+    else {
+      console.log('Ha vinto il computer')
+    }
+  }
+  else {
+    console.log('La somma dei numeri è dispari')
+    if (user_pair == 0) {
+      console.log('hai vinto tu')
+    }
+    else {
+      console.log('Ha vinto il computer')
+    }
+  }
+}
+else {
+  console.log('Hai inserito un numero sbagliato')
+}
 
 /*
 Contare quanti minuti restano per la prossima ora
